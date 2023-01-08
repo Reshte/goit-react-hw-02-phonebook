24dfd71cbc9aa22d847fb0f1ef08b43f3044e6fc
Reshte <107108359+Reshte@users.noreply.github.com>
2023-01-08 21:34:39 +0200

    edits

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,29 @@
-export const App = () => {
-  return (
-    <div
+import React, { Component } from "react";
+import { Contacts } from "./contacts/Contacts";
+
+
+export class App extends Component {
+  state = {
+  contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},],
+  name: '',
+  number: ''
+  }
+  
+  handlerAddContact = (e) => {
+    e.preventDefault()
+    console.log(e.currentTarget.firstElementChild)
+        this.setState(prevState => {
+      return( prevState )
+    })
+  }
+
+  
+  render() {
+    const {contacts,name, number} = this.state
+    
+    return (
+        <div
       style={{
         height: '100vh',
         display: 'flex',
@@ -8,9 +31,34 @@ export const App = () => {
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+      }} >
+        <form onSubmit={this.handlerAddContact}>
+          <label htmlFor="" > Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required/>
+          </label>
+       
+   <label htmlFor=""> Number
+      <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+   </label>
+        <button type="submit">Add contact</button>       
+        </form>
+        < Contacts contactsList={contacts} />
+        
+</div>
+  )    
+  }
+}
+
+
+
---
+++ src/components/contacts/Contacts.jsx
@@ -0,0 +1,15 @@
+// import PropTypes from 'prop-types';
+import React from "react";
+
+export const Contacts = ({ contactsList }) => {
+    console.log(contactsList)
+    //  return (    
+    //          {contactsList.map(friend => {
+    //                return (
+    //                    <FriendListItem
+    //                     key= {friend.id}
+    //               />)
+    //          })}
+       
+    // )
+}
