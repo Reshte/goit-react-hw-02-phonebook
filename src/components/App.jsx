b3b7d108d176aa77f92f61d0be79829eac78627b
Reshte <107108359+Reshte@users.noreply.github.com>
2023-01-09 14:51:18 +0200

    edits

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,54 @@
 import React, { Component } from "react";
 import { Contacts } from "./contacts/Contacts";
 import Form from 'components/form/form'
+import { Filter } from "./filter/Filter";
 
 const shortid = require('shortid');
 const contactId = shortid.generate();
  
 export class App extends Component {
   state = {
-  contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},],
-  name: '',
-  number: ''
-  }
+  contacts: [ {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+    filter: '',
+    }
   
   handelFormSbmit = data => {
+    
+   const isNameInContact = this.state.contacts.find(contact=> contact.name === data.name.toLowerCase().trim())
+   if(isNameInContact){
+    alert(`${contact.name}`)
+   }
+
     const contact = {
       id: contactId,
       name: data.name,
       number: data.number,
     };
 
-    this.setState(prevState => ({
+
+
+  this.setState(prevState => ({
       contacts: [...prevState.contacts, contact],
     }));
-   }
+      
+    }
  
+  filterContact = (e) => {
+        this.setState({filter: e.currentTarget.value})
+      }
+   
   deleteContact = id => {
     this.setState(prevState => ({ contacts: prevState.contacts.filter(contact => contact.id !== id) }))
      }
   
   render() {
-    const {contacts} = this.state
-    
+    const { contacts, filter } = this.state
+    const filterNormalise = filter.toLowerCase().trim()
+    const filterElemens = contacts.filter(contact => contact.name.toLowerCase().includes(filterNormalise))
+      
     return (
         <div
       style={{
@@ -40,11 +57,12 @@ export class App extends Component {
           flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
+        fontSize: 20,
         color: '#010101'
       }} >
         <Form onSubmit={this.handelFormSbmit} />
-        <Contacts contactsList={contacts} onDeleteContact={this.deleteContact} />
+        <Filter value={filter} onChange={this.filterContact } />
+        <Contacts contactsList={filterElemens} onDeleteContact={this.deleteContact} />
         
 </div>
   )    
--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,14 +1,12 @@
 // import { PropTypes } from 'prop-types';
 
-export const Filter = ({ }) => {
-    return (
+export const Filter = ({ value, onChange }) => {
+       return (
         <div>
             <label htmlFor="">Find contacts name
-                <input type="text" name="name"  />
+                <input type="text" value={value} onChange={onChange} />
             </label>
-                      
         </div>
     )
 }
 
-//value={name}
